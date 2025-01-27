573d7b3128fa4ad50883519edad09ba3e1b2988f
Brandon <kongbrandon0@gmail.com>
2025-01-26 20:56:32 -0600

    feat(service): added Base Service class and DataRetrievalService

--- brainstem_application/main.py
+++ brainstem_application/main.py
@@ -1,5 +1,17 @@
+from lib.logger import Logger, LogLevel
+from constants import (
+    ROOT_DIR
+)
+
+
+LOG_LEVEL_CONTEXT = LogLevel.DEBUG.name
+
 def main():
-    pass
+    logger = Logger(log_file=f"{ROOT_DIR}/logs/activity.log", log_level=LOG_LEVEL_CONTEXT)
+    logger.debug("Starting application")
+
+    
+    logger.debug("Ending application")
 
 
 if __name__ == "__main__":
---
+++ brainstem_application/services/data/data_retrieval_service.py
@@ -0,0 +1,18 @@
+""" Data Retrieval Service
+This module contains the DataRetrievalService class, which is a service that retrieves data from a data source.
+"""
+
+from brainstem_application.services.base import Service
+
+class DataRetrievalService(Service):
+    def __init__(self):
+        super().__init__("Data Retrieval Service")
+
+    def docs(self):
+        return "This service retrieves data from a data source."
+
+    def startup(self):
+        pass
+
+    def cleanup(self):
+        pass
